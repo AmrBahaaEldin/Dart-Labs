508ac55ffcea7cfba90db2463507d13e7f0cf04e
Amr_BahaaEldin <108246015+AmrBahaaEldin@users.noreply.github.com>
2024-08-21 01:26:04 +0300

    Add files via upload

---
+++ primitive_data_type/lib/main.dart
@@ -0,0 +1,32 @@
+void main() {
+  print("hello world "); //print
+  num x = 5; //declare and assign  _
+  num y = 5.6; //num  using know n't enter  type variable
+  print(y); //print
+  print(x); //print
+  //data type
+  String fristName = "Amr";
+  String lastName = "Mohamed";
+  int salary = 500;
+  print(salary);
+  double tax = 14.5;
+  print(tax);
+  //dot operation
+  print(fristName.length);
+  print(lastName.toUpperCase());
+  //Arithmetic operators
+  print(salary ~/ tax); //take real number only
+  bool hoilday = false;
+  print(hoilday);
+  // Dynamic data type
+  dynamic noThings = "amrBahaaEldin";
+  noThings = 11111; //change data type
+  print(noThings);
+  //var is keyword not dataType but can't change data type
+  var name = "amr";
+  name = "bhaaaEldin";
+  print(name);
+  var number = 001012002;
+  print(number);
+  
+}
