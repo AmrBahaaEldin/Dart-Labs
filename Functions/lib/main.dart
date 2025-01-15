464fa2d4b6761e8d98a17e1216c4b2985c116480
Amr_BahaaEldin <108246015+AmrBahaaEldin@users.noreply.github.com>
2025-01-15 08:51:46 +0200

    Add files via upload

---
+++ Functions/lib/main.dart
@@ -0,0 +1,27 @@
+void main() {
+  //parameters is  in fun(,) variables with datatype
+  //arguments is  data using call funcation 
+  //optional is [],{}
+
+  int z = namelist(); //function now reply answer
+  // prints "hello my friend"
+  //funcation
+
+  double finalResult = funcNamed(10, 20);
+  print(finalResult); // prints 30.0
+
+
+  print(z + 10); // prints 20 should using return
+}
+
+int namelist() {
+  int x = 5;
+  int y = 10;
+  int z = x + y;
+  return (z);
+}
+
+double funcNamed(double x, double y) {
+  return x + y;
+}
+////////////////////////////////////////////////
